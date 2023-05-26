2a1aa592fd4b4e49f68ed9a2ace6a0240e2fe35f
Eric Passmore <eric.passmore@gmail.com>
2023-05-25 17:19:42 -0700

    fixed srv path

--- git/cross_host_sync.sh
+++ git/cross_host_sync.sh
@@ -6,4 +6,4 @@ WORKING_DIR=$(pwd)
 ORG_DIR=$(echo ${WORKING_DIR} | cut -d"/" -f6)
 REPO=$(echo ${WORKING_DIR} | cut -d"/" -f7)
 
-rsync -av "$C_ROOT_DIR/$ORG_DIR/$REPO" "$S_ROOT_DIR/$ORG_DIR/$REPO"
+rsync -av "$C_ROOT_DIR/$ORG_DIR/$REPO" "$S_ROOT_DIR/$ORG_DIR/"
