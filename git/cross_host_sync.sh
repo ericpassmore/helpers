83a57cf2b3ef07a12eac6cbeccce93f686b01030
Eric Passmore <eric.passmore@gmail.com>
2023-05-25 17:17:02 -0700

    simplified client to srv

--- git/cross_host_sync.sh
+++ git/cross_host_sync.sh
@@ -1,39 +1,9 @@
 #!/usr/bin/env bash
 
-IP=${1:-"127.0.0.1"}
-if [ "$IP" == "127.0.0.1" ]; then echo "BAD CLIENT IP RESET"; exit; fi
+C_ROOT_DIR="/Users/eric/eosnetworkfoundation/repos"
+S_ROOT_DIR="/Users/eric/superbee/local/eosnetworkfoundation/repos"
+WORKING_DIR=$(pwd)
+ORG_DIR=$(echo ${WORKING_DIR} | cut -d"/" -f6)
+REPO=$(echo ${WORKING_DIR} | cut -d"/" -f7)
 
-echo "please choose an org ENF, Wharf, Antelope, EHP"
-read ORGANIZATION
-
-case $ORGANIZATION in
-  ehp)
-    GIT_ORG="ericpassmore"
-    DIR=$GIT_ORG
-    ;;
-  wharf)
-    GIT_ORG="wharfkit"
-    DIR="wharf"
-    ;;
-  antelope)
-    GIT_ORG="antelopeIO"
-    DIR="antelope"
-    ;;
-  enf)
-    GIT_ORG="eosnetworkfoundation"
-    DIR="ENF"
-    ;;
-  *)
-    echo "option not valid"; exit 1;
-    ;;
-esac
-echo "please choose a repo"
-ROOT_DIR="/local/eosnetworkfoundation/repos"
-cd "${ROOT_DIR:?}/$DIR" || exit
-ls -1
-read REPO
-cd "${REPO:?}" || exit
-echo "enter branch to sync"
-git branch -a
-read BRANCH
-git pull origin "$BRANCH" || exit
+rsync -av "$C_ROOT_DIR/$ORG_DIR/$REPO" "$S_ROOT_DIR/$ORG_DIR/$REPO"
