a50637748924caad70d9e8455de630e2ce32aa2e
Eric Passmore <eric.passmore@gmail.com>
2024-01-17 16:33:40 -0800

    fix quote wrap

--- nodeos/bin/hash_and_sign_release.sh
+++ nodeos/bin/hash_and_sign_release.sh
@@ -109,7 +109,7 @@ print (json.load(sys.stdin)['download_url'])")
      echo "<td>${PR_NUM}</td> " >> "${HTML_ROOT}"/leap/leap-build-history.txt
      echo "<td>${PR_TITLE}</td>" >> "${HTML_ROOT}"/leap/leap-build-history.txt
      echo "<td><a href='${DOWNLOAD_URL}'>Download Deb</a></td>" >> "${HTML_ROOT}"/leap/leap-build-history.txt
-     cp "${LEAP_BUILD_DIR}/signed-outputs/local/leap_*.deb.asc" "${HTML_ROOT}"/leap/signatures/${GIT_SHORT_SHA}.asc
+     cp "${LEAP_BUILD_DIR}"/signed-outputs/local/leap_*.deb.asc "${HTML_ROOT}"/leap/signatures/${GIT_SHORT_SHA}.asc
      echo '<!DOCTYPE html><html><body>' > "${HTML_ROOT}"/leap/verified-builds.html
      echo '<table><thead><tr><th>Merge Time</th><th>Branch</th><th>Git Commit</th><th>PR Num</th>' >> "${HTML_ROOT}"/leap/verified-builds.html
      echo '<th>PR Title</th><th>Signature</th></tr></thead>' >> "${HTML_ROOT}"/leap/verified-builds.html
