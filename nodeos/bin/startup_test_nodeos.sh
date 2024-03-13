46b475ab180c308e5a962663713eb18733ab5ae5
Eric Passmore <eric.passmore@gmail.com>
2024-03-13 15:56:17 -0700

    fix path clean up

--- nodeos/bin/startup_test_nodeos.sh
+++ nodeos/bin/startup_test_nodeos.sh
@@ -130,11 +130,11 @@ start_nodeos_from_genesis() {
 # clean out data files
 ######
 clean_nodeos() {
-  [ -f "$ROOT_DIR"/nodeos/data/blocks/blocks.log ] && rm -f "$ROOT_DIR"/nodeos/data/blocks/blocks.log
-  [ -f "$ROOT_DIR"/nodeos/data/blocks/blocks.index ] && rm -f "$ROOT_DIR"/nodeos/data/blocks/blocks.index
-  [ -f "$ROOT_DIR"/nodeos/data/state/shared_memory.bin ] && rm -f "$ROOT_DIR"/nodeos/data/state/shared_memory.bin
-  [ -f "$ROOT_DIR"/nodeos/data/state/code_cache.bin ] && rm -f "$ROOT_DIR"/nodeos/data/state/code_cache.bin
-  [ -f "$ROOT_DIR"/nodeos/data/blocks/reversible/fork_db.dat ] && rm -f "$ROOT_DIR"/nodeos/data/blocks/reversible/fork_db.dat
+  [ -f "$DATA_DIR"/blocks/blocks.log ] && rm -f "$DATA_DIR"/blocks/blocks.log
+  [ -f "$DATA_DIR"/blocks/blocks.index ] && rm -f "$DATA_DIR"/blocks/blocks.index
+  [ -f "$DATA_DIR"/state/shared_memory.bin ] && rm -f "$DATA_DIR"/state/shared_memory.bin
+  [ -f "$DATA_DIR"/state/code_cache.bin ] && rm -f "$DATA_DIR"/state/code_cache.bin
+  [ -f "$DATA_DIR"/blocks/reversible/fork_db.dat ] && rm -f "$DATA_DIR"/blocks/reversible/fork_db.dat
 }
 
 ## Source Config, provides the following:
