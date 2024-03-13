5c4dfb7be0b582315fd29870bb5611246811a592
Eric Passmore <eric.passmore@gmail.com>
2024-03-13 14:25:57 -0700

    bug fix docker setup scripts for leap

--- nodeos/bin/docker-run-container.sh
+++ nodeos/bin/docker-run-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -d -it --name ${1:-leap-vIF} -v ${HOME}:/docker-mount --entrypoint /bin/bash leap-base:05
-# docker exec -it leap-vIF /bin/bash
+docker run -d -it --name ${1:-leap-v5} -v ${HOME}:/docker-mount --entrypoint /bin/bash leap-base:05
+# docker exec -it leap-v5 /bin/bash
--- nodeos/bin/finish-setup-docker-container.sh
+++ nodeos/bin/finish-setup-docker-container.sh
@@ -3,10 +3,10 @@
 CDT_GIT_COMMIT_TAG=${1:-v4.0.1}
 
 echo "LINKING HELPER BIN DIR"
-ln -s /local/eosnetworkfoundation/repos/ericpassmore/helpers/nodeos/bin /local/eosnetworkfoundation/bin
-source ${ROOT_DIR:?}/bin/nodeos_config.sh
+ln -s /local/eosnetworkfoundation/repos/helpers/nodeos/bin /local/eosnetworkfoundation/bin
+source /local/eosnetworkfoundation/bin/nodeos_config.sh
 
-cd "${LEAP_GIT_DIR:?}"/deb || exit
+cd "${ROOT_DIR:?}"/deb || exit
 echo "GET LEAP DEB"
 wget https://github.com/AntelopeIO/leap/releases/download/v5.0.2/leap_5.0.2_amd64.deb
 
@@ -18,8 +18,14 @@ git pull origin $CDT_GIT_COMMIT_TAG
 
 mkdir build
 cd build || exit
-cmake .. >> "$LOG_DIR"/cdt_build_log.log 2>&1
-make -j ${NPROC} >> "$LOG_DIR"/cdt_build_log.log 2>&1
+#cmake .. >> "$LOG_DIR"/cdt_build_log.log 2>&1
+#make -j ${NPROC} >> "$LOG_DIR"/cdt_build_log.log 2>&1
 echo "FINSIHED BUILDING CDT"
 
 mkdir $WALLET_DIR $CONFIG_DIR $DATA_DIR
+
+## su to root and install
+# cd "${ROOT_DIR:?}"/deb
+# dpkg -i ./leap_[5-6].[0-9].[0-9]*-ubuntu22.04_amd64.deb
+# cd ${ROOT_DIR:?}"/repos/cdt/build || exit
+# make install
